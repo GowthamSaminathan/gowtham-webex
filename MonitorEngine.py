e722c531a8e5f07ea35ecd62cf50e1ecf0076699
root <gowthams6666@gmail.com>
2017-09-10 13:37:38 -0700

    Scoring issue fixed

--- MonitorEngine.py
+++ MonitorEngine.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import os.path
 import logging
+from logging.handlers import RotatingFileHandler
 import shutil
 from pexpect import pxssh
 import pexpect
@@ -19,16 +20,15 @@ from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-handler = logging.FileHandler('MonitorEngineLog.log')
-handler.setLevel(logging.ERROR)
+logger =  logging.getLogger("Rotating Log")
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(os.getcwd()+"/MonitorEngine.log",maxBytes=5000000,backupCount=100)
 formatter = logging.Formatter('%(asctime)s > %(levelname)s > %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.propagate = False # DISABLE LOG STDOUT
+#logger.propagate = False # DISABLE LOG STDOUT
 
-logger.info('\n'+'='*20)
+logger.info("Starting MonitoringEngine")
 
 app = Flask(__name__)
 
@@ -36,16 +36,18 @@ app = Flask(__name__)
 class score_gen():
 
     def __init__(self):
-        print("Scoring Object Insiated...")
+        logger.info("Scoring Object Insiated...")
     
     def mongo_search_score(self,INID,session,mdb):
         try:
-            print INID,session
+            logger.debug("Mongo search score for:"+str(str(INID)+" "+str(session)))
             mdb_out = mdb['OUTPUT']
             all_data = mdb_out.find({"INID":INID,"SESSION":session})
             for single_host in all_data:
                 try:
                     #print single_host
+                    IP = single_host.get("IP")
+                    logger.debug("mongo_search_score > Starting score for > "+str(IP))
                     mon_objects = single_host.get("Objects")
                     obj_index = -1
                     for mon_obj in mon_objects:
@@ -57,30 +59,31 @@ class score_gen():
                             #print rank
                             custom_query = rank.get("regex")
                             score = rank.get("score")
-                            dafault_query = {"INID":INID,"SESSION":session,"Objects.id":elmt_id}
+                            dafault_query = {"INID":INID,"SESSION":session,"IP":IP,"Objects.id":elmt_id}
                             #print custom_query
                             new_dict = {"Objects"+".out."+key: value for key, value in custom_query.items()}
                             #print new_dict
                             dafault_query.update(new_dict)
-                            print dafault_query
+                            logger.debug("mongo_search_score > searching DB for:"+str(dafault_query))
                             queryout = mdb_out.find_one(dafault_query,{"_id":0,"TD":0})
                             #print queryout
                             if queryout == None:
-                                print "Issue Pattern Not matched"
+                                logger.debug("mongo_search_score > default_query > Pattern Not matched")
                             else:
-                                print "Issue Pattern  Matched"
+                                logger.debug("mongo_search_score > default_query > Pattern Matched")
                                 # Update very lowest score in rank pattern
                                 if last_score < score:
                                     last_score = score
                                 #tmpdesc = hostname+" "+ip+" "+str(emon)+" "+str(etype)+" "+str(ename)+" "+str(custom_query)+" "+str(last_score)+" "+str(queryout.get("OUT"))
                                 #desc = desc+"\n"+str(tmpdesc)
                                 #print "Issue pattern matched > ",desc
-                                print dafault_query
+                                d = dafault_query,{"$set": {"Objects.$"+".score":last_score}}
+                                logger.debug("mongo_search_score > updating score >"+str(d))
                                 mdb_out.update(dafault_query,{"$set": {"Objects.$"+".score":last_score}})
                 except Exception as e:
-                    print("mongo_search_score Error Position1:>"+str(e))
+                    logger.exception("mongo_search_score")
         except Exception as e:
-            print("mongo_search_score Error Position0:>"+str(e))
+            logger.exception("mongo_search_score")
 
 ### PAGE 1 END ####
 
@@ -94,7 +97,7 @@ class main_model():
         # Login to NPCI device , "enable" password check disabled because of aaa conf in NPCI
         if len(auth) > 0:
             for au in auth:
-                print ("Trying to Login:"+hostname)
+                logger.info("Trying to Login:"+hostname)
                 return_typ = None
                 username = au.get("username")
                 password = au.get("password")
@@ -115,21 +118,21 @@ class main_model():
                         aftr = s.after
                         if type(aftr) == str:
                             host_name = host_name+aftr.strip().decode("utf-8")
-                        print("Login Success :"+hostname+":"+host_name)
+                        logger.info("Login Success :"+hostname+":"+host_name)
                         return s,host_name
                     else:
-                        print("Not able to reach device:"+hostname)
+                        logger.info("Not able to reach device:"+hostname)
                     return "TIMEOUT"
                 except pxssh.ExceptionPxssh as e:
                     err = str(e)
                     if err.find("password refused") != -1:
-                        print("Login Failed:"+hostname)
+                        logger.info("Login Failed:"+hostname)
                         return_typ = "LOGINFAIL"
                     else:
-                        print("Error>"+err+":"+hostname)
+                        logger.info("Login Error>"+err+":"+hostname)
                         return "TIMEOUT"
                 except Exception as e:
-                    print("Unknown Error :"+str(e))
+                    logger.exception("login")
                     return "TIMEOUT"
             return return_typ
 
@@ -178,7 +181,8 @@ class main_model():
             if Monitoring_obj == None :
                 # Objects Not Found
                 return None
-            print Monitoring_obj
+            logger.debug("single host object")
+            logger.debug(str(Monitoring_obj))
             for element in Monitoring_obj:
                 #Object found ; reading elements
                 et = element.get("type")
@@ -188,7 +192,7 @@ class main_model():
             
             return Monitoring_obj
         except Exception as e:
-            print("Error >"+str(e))
+            logger.exception("device_check")
 
     def single_host(self,row2,dir_path):
         #Login and run
@@ -209,12 +213,12 @@ class main_model():
             mcollection.update({"_id":1,"STATUS.IP":IP},{ "$set": { "STATUS.$.TYPE" : "Running" } })
             mongoc.close()
         except Exception as e:
-            print("Error > single_host"+str(e))
+            logger.exception("single_host")
 
         sess = self.get_ssh_ses(IP,[Authentication],timeout,dir_path)
         jout = {}
         if sess == None or type(sess) == str:
-            print("Failed >"+str(Hostname)+" "+str(IP))
+            logger.info("Failed >"+str(Hostname)+" "+str(IP))
             def_rnk = [{"regex":{"status":"down"},"score":0},{"regex":{"status":"reachable"},"score":100}]
             jout = [{"monitor" : "self","type" : "self_check","name" : "self","rank":def_rnk,"score" : 0,"out":{"status":"down"}}]
         else:
@@ -232,13 +236,13 @@ class main_model():
             if self.mongdb(TD,"xls",input_file_path) == True:
                 pass;
             else:
-                print("STOPPED")
+                logger.error("STOPPED")
                 return
             session = 0
             for y in range(1):
                 tim = time.strftime('%Y-%m-%d %H:%M:%S')
                 session = session + 1
-                print("STARTING SESSION >"+str(session))
+                logger.info("STARTING SESSION >"+str(session))
                 mcollection = self.mdb['SESSION']
                 sesout = mcollection.find_one({"_id":1})
                 INID = sesout.get("INID")
@@ -252,7 +256,7 @@ class main_model():
                 if not os.path.exists(dir_path):
                     os.makedirs(dir_path)
                 dir_path = os.path.join(dir_path,jobname)
-                print "Apprentice>"+str(apprentice)
+                logger.info("Apprentice>"+str(apprentice))
                 # Share work to threads
                 with ThreadPoolExecutor(max_workers=apprentice) as executor:
                     futures = [executor.submit(self.single_host, row,dir_path) for row in all_data]
@@ -269,20 +273,19 @@ class main_model():
                             mcollection = self.mdb['SESSION']
                             mcollection.update({"_id":1,"STATUS.IP":jout.get("IP")},{ "$set": { "STATUS.$.TYPE" : "Completed" } })
                         except Exception as e:
-                            print("start_run trying Error>>"+str(e))
+                            logger.exception("start_run")
                 
                 #Start Scoring
-                print("Scoring Started")
+                logger.info("Scoring Started")
                 self.mongo_search_score(INID,session,self.mdb)
-                print("Scoring Completed")
+                logger.info("Scoring Completed")
                 #UPDATE CURRENT SESSION
                 mcollection = self.mdb['SESSION']
                 mcollection.update({"_id":1},{"$set":{"SESSION":session}})
                 mcollection = self.mdb['HISTORY']
                 mcollection.insert({"INID":int(INID),"SESSION":int(session),"TD":TD,"JOBNAME":jobname})
         except Exception as e:
-            logger.error(str(e))
-            print("start_run Error >"+str(e))
+            logger.exception("start_run")
 
     def xls_input(self,filename):
         # Ready XLS input file and formate to JSON for inserting to MongoDB
@@ -307,8 +310,7 @@ class main_model():
                     "timeout":int(xx["timeout"]),"Objects":local_list})
             return full_list
         except Exception as e:
-            logger.error(str(e))
-            print("xls_input Error>"+str(e))
+            logger.exception("xls_input")
     
     def mongdb(self,TD,input="xls",filepath=None):
         try:
@@ -316,7 +318,7 @@ class main_model():
             if input == "xls":
                 csv_data = self.xls_input(filepath)
                 if csv_data == None or len(csv_data) == 0:
-                    print("No valid XLS input")
+                    logger.error("No valid XLS input")
                     return None
             else:
                 return None
@@ -343,28 +345,29 @@ class main_model():
             mcollection = self.mdb['SESSION']
             #print list(ses)
             mcollection.update({"_id":1},{"_id":1,"INID":INID,"SESSION":0,"JOBNAME":self.jobname,"STATUS":list(ses),"STARTDATE":TD})
-            print("SESSION UPDATED , INID = "+str(INID))
+            logger.info("SESSION UPDATED , INID = "+str(INID))
             
             #self.mongoc.close()
             return True
         except Exception as e:
-            print(e,"Error mongdb")
+            logger.exception("mongdb")
     
     def main_run(self,filepath,jobname,apprentice):
         # connect to LIVE database
         self.mongoc = pymongo.MongoClient('localhost', 27017)
         self.mdb = self.mongoc['LIVE']
-        print ("Connected to 'LIVE' database...")
+        logger.info("Connected to 'LIVE' database...")
 
         # Insitate Score Me object
         scor = score_gen()
         self.mongo_search_score = scor.mongo_search_score
 
-        print ("Starting...")
+        logger.info("Starting Job ===============> "+str(jobname))
         self.start_run(filepath,jobname,apprentice)
 
 
 if __name__ == '__main__':
-    m = main_model()
-    m.main_run("/home/gow/network_snap/input_file/06-August-08_41_07-GNS.xlsx","TESTTTT",5)
     pass;
+    logger.info("Manual Mode Running")
+    #m = main_model()
+    #m.main_run("/home/gow/network_snap/input_file/06-August-08_41_07-GNS.xlsx","TESTTTT",5)
